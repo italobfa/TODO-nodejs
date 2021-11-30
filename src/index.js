52d54311d7a696b387d27938b130a4c695b74a2c
italobfa <italobfa@hotmail.com>
2021-11-29 21:30:34 -0300

    create user

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-const express = require('express')
+const express = require("express");
 
-const {v4: uuidv4} = require('uuid')
+const { v4: uuidv4 } = require("uuid");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+const users = [];
+
+app.post("/users", (request, response) => {
+  const { name, username } = request.body;
+  const userExists = users.find((user) => user.username === username);
+
+  if (userExists) {
+    return response.status(400).json({ error: "Username already exists" });
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todo: [],
+  }
+
+  users.push(user);
+
+  return response.status(201).json(user)
+});
+
+app.listen(3333);
